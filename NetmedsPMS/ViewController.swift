bfd37e64f1a1096e4a9bb04ce76058119b591271
Amal Mishra <amalm@mindfiresolutions.com>
2020-05-02 17:12:25 +0530

    Initial Commit

---
+++ NetmedsPMS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetmedsPMS
+//
+//  Created by Amal Mishra on 02/05/20.
+//  Copyright © 2020 Radhika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NetmedsPMSTests/NetmedsPMSTests.swift
@@ -0,0 +1,34 @@
+//
+//  NetmedsPMSTests.swift
+//  NetmedsPMSTests
+//
+//  Created by Amal Mishra on 02/05/20.
+//  Copyright © 2020 Radhika. All rights reserved.
+//
+
+import XCTest
+@testable import NetmedsPMS
+
+class NetmedsPMSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
